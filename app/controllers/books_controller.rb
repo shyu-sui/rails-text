b89601089a926670cab45910a78b6ed79f20432f
shyu-sui <iori121380@gmail.com>
2021-01-16 10:48:50 +0000

    2021/1/16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id)
+    redirect_to book_path(@book.id) , notice: 'Book was successfully updated.' 
     else
     @books = Book.all
     render :index
@@ -32,7 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id) , notice: 'Book was successfully updated.'
     else
       render :edit
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_01_09_103950) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
